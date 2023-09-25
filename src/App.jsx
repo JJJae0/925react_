96caaba5b82f9ed6ac14c9d60d16a30dd1576963
JJJae0 <jaeyeong345@naver.com>
2023-09-25 16:34:50 +0900

    버튼으로 색상 변경하기

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,18 @@ import './style.scss';
 // 리액트 안에서 배열이나, 객체 같은 참조형 자료는 무조건 depp copy 를 해서 데이터를 변경해야됨
 
 function App() {
-	const [Degree, setDegree] = useState(0);
+	const [Colors, setColors] = useState(['red', 'green', 'blue']);
+	const newColors = [...Colors];
+	newColors[0] = 'hotpink';
 
 	return (
 		<>
-			<button onClick={() => setDegree(Degree - 45)}>왼쪽으로 회전</button>
-			<button onClick={() => setDegree(Degree + 45)}>오른쪽으로 회전</button>
-			<article style={{ transform: `rotate(${Degree}deg)` }}>{Degree}</article>
+			{Colors.map((color, idx) => (
+				<button style={{ backgroundColor: color }} key={idx}>
+					{color}
+				</button>
+			))}
+			<button onClick={() => setColors(newColors)}>색상변경</button>
 		</>
 	);
 }
